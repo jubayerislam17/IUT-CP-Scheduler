4350a90d1bcc32f4d75d62bfd4039847d4f2e45b
Mahtab Nur Fardin <mahtabnur@iut-dhaka.edu>
2023-06-03 15:21:49 +0600

    converted to pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter} from 'react-router-dom'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { AuthContextProvider } from './context/AuthContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +19,12 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 ///before adding context 
 // root.render(
